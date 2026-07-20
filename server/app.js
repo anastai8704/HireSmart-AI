99872d69f1fe610a94fa594a42c8e9a02318909a
Anas <tanastai5018@gmail.com>
2026-07-20 23:29:53 +0530

    Complete Lesson 5 - Professional Project Structure

--- server/app.js
+++ server/app.js
@@ -2,24 +2,8 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 5000;
-
 app.get("/", (req, res) => {
     res.send("Welcome to HireSmart AI");
 });
 
-app.get("/about", (req, res) => {
-    res.send("About HireSmart AI");
-});
-
-app.get("/jobs", (req, res) => {
-    res.send("List of Jobs");
-});
-
-app.get("/login", (req, res) => {
-    res.send("Login Page");
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
