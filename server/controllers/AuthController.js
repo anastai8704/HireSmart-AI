8929b95589007c5ff3e9e8e573463672936e5143
Anas <tanastai5018@gmail.com>
2026-07-21 22:27:46 +0530

    Complete Lesson 10 - Login API with JWT

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
     try {
@@ -34,15 +35,95 @@ exports.register = async (req, res) => {
             password: hashedPassword
         });
 
+        // Response object
+        const userResponse = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            phone: user.phone,
+            profileImage: user.profileImage,
+        };
+
         return res.status(201).json({
             success: true,
             message: "User Registered Successfully",
-            user
+            user: userResponse,
+        });
+
+    } catch (error) {
+        console.error("Register Error:", error);
+
+        return res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+    }
+};
+exports.login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        // Validation
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and Password are required"
+            });
+        }
+
+        // Check user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Password"
+            });
+        }
+
+        // Generate Token
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        const userResponse = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            phone: user.phone,
+            profileImage: user.profileImage,
+        };
+
+        return res.status(200).json({
+            success: true,
+            message: "Login Successful",
+            token,
+            user: userResponse
         });
 
     } catch (error) {
 
-        console.log(error);
+        console.error("Login Error:", error);
 
         return res.status(500).json({
             success: false,
